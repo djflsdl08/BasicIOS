425c0d0b9552bab0c152f4e28459a2047f90f2d8
djflsdl08 <djflsdl08@naver.com>
2017-10-18 13:42:03 +0900

    Added imagePicker.

--- FoodTracker/FoodTracker/ViewController.swift
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate,
+    UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     //MARK : Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var phtoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,6 +36,28 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         mealNameLabel.text = "Default Text"
+    }
+    
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        nameTextField.resignFirstResponder()    // Hide keyboard
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    // MARK : UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated : true, completion : nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage
+            else {
+                fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        phtoImageView.image = selectedImage
+        dismiss(animated : true, completion : nil)
     }
 }
 
