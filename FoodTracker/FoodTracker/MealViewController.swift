6fb95cd5088b9bf76816301e6b7c74be059a16e8
djflsdl08 <djflsdl08@naver.com>
2017-10-19 18:13:22 +0900

    Added support to edit and delete meals from the meal list.

--- FoodTracker/FoodTracker/MealViewController.swift
+++ FoodTracker/FoodTracker/MealViewController.swift
@@ -23,6 +23,14 @@ class MealViewController: UIViewController, UITextFieldDelegate,
     override func viewDidLoad() {
         super.viewDidLoad()
         nameTextField.delegate = self
+        
+        if let meal = meal {
+            navigationItem.title = meal.name
+            nameTextField.text = meal.name
+            phtoImageView.image = meal.photo
+            RatingControl.rating = meal.rating
+        }
+        
         updateSaveButtonState()
     }
 
@@ -45,7 +53,15 @@ class MealViewController: UIViewController, UITextFieldDelegate,
     
     //MARK: Navigation
     @IBAction func cancel(_ sender: UIBarButtonItem) {
-        dismiss(animated: true, completion: nil)
+        let isPresentingInAddMealMode = presentingViewController is UINavigationController
+        
+        if isPresentingInAddMealMode {
+            dismiss(animated: true, completion: nil)
+        } else if let owningNavigationController = navigationController {
+            owningNavigationController.popViewController(animated: true)
+        } else {
+            fatalError("The MealViewController is not inside a navigation controller")
+        }
     }
     
     override func prepare(for segue : UIStoryboardSegue, sender: Any?) {
