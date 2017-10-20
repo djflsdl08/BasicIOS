9d6e8e939ca9d0ccb32b5b69c0fdd9cb833af234
djflsdl08 <djflsdl08@naver.com>
2017-10-20 22:01:20 +0900

    Create Login System.

---
+++ LoginSystem/LoginSystem/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginViewController.swift
+//  LoginSystem
+//
+//  Created by 김예진 on 2017. 10. 20..
+//  Copyright © 2017년 Kim,Yejin. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
