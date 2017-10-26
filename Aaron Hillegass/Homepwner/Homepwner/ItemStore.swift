6ff299252ee57cb7ac1dbb1d077f22c9f082d336
djflsdl08 <djflsdl08@naver.com>
2017-10-27 00:39:34 +0900

    Chap10 - Edit UITableView.

--- Aaron Hillegass/Homepwner/Homepwner/ItemStore.swift
+++ Aaron Hillegass/Homepwner/Homepwner/ItemStore.swift
@@ -11,17 +11,36 @@ import UIKit
 class ItemStore {
     var allItems = [Item]()
     
+    /* Don't need anymore because of 'Edit' and 'Add' buttons.
     init() {
         for _ in 0..<5 {
             let _ = createItem()
         }
     }
-    
+    */
+ 
     func createItem() -> Item {
         let newItem = Item(random: true)
         allItems.append(newItem)
         
         return newItem
+    }
+    
+    func removeItem(item: Item) {
+        if let index = allItems.index(of: item) {
+            allItems.remove(at: index)
+        }
+    }
+    
+    func moveItemAtIndex(fromIndex: Int, toIndex: Int) {
+        if fromIndex == toIndex {
+            return
+        }
+        
+        let movedItem = allItems[fromIndex]
+        
+        allItems.remove(at: fromIndex)
+        allItems.insert(movedItem, at: toIndex)
     }
     
 }
--- Aaron Hillegass/Homepwner/Homepwner/ItemsTableViewController.swift
+++ Aaron Hillegass/Homepwner/Homepwner/ItemsTableViewController.swift
@@ -12,6 +12,28 @@ class ItemsTableViewController: UITableViewController {
     
     var itemStore : ItemStore!
     
+    @IBAction func addNewItem(_ sender: UIButton) {
+        let newItem = itemStore.createItem()
+        
+        if let index = itemStore.allItems.index(of: newItem) {
+            let indexPath = NSIndexPath(row: index, section: 0)
+                
+            tableView.insertRows(at: [indexPath as IndexPath], with: .automatic)
+        }
+    }
+    
+    @IBAction func toggleEditingMode(_ sender: UIButton) {
+        if isEditing {
+            sender.setTitle("Edit", for: .normal)
+            
+            setEditing(false, animated: true)
+        } else {
+            sender.setTitle("Done", for: .normal)
+            
+            setEditing(true, animated: true)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,7 +47,6 @@ class ItemsTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return (itemStore.allItems.count + 1)
     }
-    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
@@ -51,6 +72,56 @@ class ItemsTableViewController: UITableViewController {
         } else {
             return 60
         }
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            if indexPath.row == itemStore.allItems.count {
+                let alert = UIAlertController (
+                                title: "Information",
+                                message: "You can't delete this row.",
+                                preferredStyle: UIAlertControllerStyle.alert
+                            )
+                let okAction = UIAlertAction (
+                                    title: "OK",
+                                    style: UIAlertActionStyle.default
+                                )
+                alert.addAction(okAction)
+                present(alert, animated: true, completion: nil)
+            } else {
+                let item = itemStore.allItems[indexPath.row]
+            
+                let title = "Delete \(item.name)"
+                let message = "Are you sure you want to delete this item?"
+                
+                let actionSheet = UIAlertController (
+                                        title: title,
+                                        message : message,
+                                        preferredStyle: .actionSheet
+                                    )
+                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+                actionSheet.addAction(cancelAction)
+                
+                let deleteAction = UIAlertAction (
+                                        title: "Delete",
+                                        style: .destructive,
+                                        handler: {
+                                            action -> Void in
+                                                self.itemStore.removeItem(item: item)
+                                                self.tableView.deleteRows(
+                                                    at: [indexPath as IndexPath],
+                                                    with: .automatic
+                                                )
+                                        }
+                                    )
+                actionSheet.addAction(deleteAction)
+                present(actionSheet, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        itemStore.moveItemAtIndex(fromIndex: sourceIndexPath.row, toIndex: destinationIndexPath.row)
     }
     
 }
