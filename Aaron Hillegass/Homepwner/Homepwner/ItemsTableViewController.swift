c86b3fb8e0afe8d9a3fa818ba5d77c9b4eb2e748
djflsdl08 <djflsdl08@naver.com>
2017-10-27 16:39:25 +0900

    Prevent repositioning of last row.

--- Aaron Hillegass/Homepwner/Homepwner/ItemsTableViewController.swift
+++ Aaron Hillegass/Homepwner/Homepwner/ItemsTableViewController.swift
@@ -17,7 +17,7 @@ class ItemsTableViewController: UITableViewController {
         
         if let index = itemStore.allItems.index(of: newItem) {
             let indexPath = NSIndexPath(row: index, section: 0)
-                
+            
             tableView.insertRows(at: [indexPath as IndexPath], with: .automatic)
         }
     }
@@ -51,7 +51,7 @@ class ItemsTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
         
-        if indexPath.row == itemStore.allItems.count {
+        if isEqualToAllItemsCount(row: indexPath.row) {
             cell.textLabel?.text = "No more items!"
             cell.detailTextLabel?.text = ""
         } else {
@@ -67,61 +67,107 @@ class ItemsTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if indexPath.row == itemStore.allItems.count {
+        if isEqualToAllItemsCount(row: indexPath.row) {
             return 44
         } else {
             return 60
         }
     }
     
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            if indexPath.row == itemStore.allItems.count {
-                let alert = UIAlertController (
-                                title: "Information",
-                                message: "You can't delete this row.",
-                                preferredStyle: UIAlertControllerStyle.alert
-                            )
-                let okAction = UIAlertAction (
-                                    title: "OK",
-                                    style: UIAlertActionStyle.default
-                                )
-                alert.addAction(okAction)
-                present(alert, animated: true, completion: nil)
-            } else {
-                let item = itemStore.allItems[indexPath.row]
-            
-                let title = "Delete \(item.name)"
-                let message = "Are you sure you want to delete this item?"
-                
-                let actionSheet = UIAlertController (
-                                        title: title,
-                                        message : message,
-                                        preferredStyle: .actionSheet
-                                    )
-                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-                actionSheet.addAction(cancelAction)
-                
-                let deleteAction = UIAlertAction (
-                                        title: "Delete",
-                                        style: .destructive,
-                                        handler: {
-                                            action -> Void in
-                                                self.itemStore.removeItem(item: item)
-                                                self.tableView.deleteRows(
-                                                    at: [indexPath as IndexPath],
-                                                    with: .automatic
-                                                )
-                                        }
-                                    )
-                actionSheet.addAction(deleteAction)
-                present(actionSheet, animated: true, completion: nil)
-            }
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        if isEqualToAllItemsCount(row: indexPath.row) {
+            return UITableViewCellEditingStyle.none
+        }
+        else {
+            return UITableViewCellEditingStyle.delete
         }
     }
     
-    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            /*if isEqualToAllItemsCount(row: indexPath.row) {
+                let alert = UIAlertController (
+                    title: "Information",
+                    message: "You can't delete this row.",
+                    preferredStyle: UIAlertControllerStyle.alert
+                    )
+                let okAction = UIAlertAction (
+                    title: "OK",
+                    style: UIAlertActionStyle.default
+                    )
+                alert.addAction(okAction)
+                present(alert, animated: true, completion: nil)
+            } else {*/
+            let item = itemStore.allItems[indexPath.row]
+            
+            let title = "Delete \(item.name)"
+            let message = "Are you sure you want to delete this item?"
+            
+            let actionSheet = UIAlertController (
+                title: title,
+                message : message,
+                preferredStyle: .actionSheet
+            )
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            actionSheet.addAction(cancelAction)
+            
+            let deleteAction = UIAlertAction (
+                title: "Remove",
+                style: .destructive,
+                handler: {
+                    action -> Void in
+                    self.itemStore.removeItem(item: item)
+                    self.tableView.deleteRows(
+                        at: [indexPath as IndexPath],
+                        with: .automatic
+                    )
+            }
+            )
+            actionSheet.addAction(deleteAction)
+            present(actionSheet, animated: true, completion: nil)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return !isEqualToAllItemsCount(row: indexPath.row)
+    }
+    
+    override func tableView(_ tableView: UITableView, targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath, toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
+        if isEqualToAllItemsCount(row: proposedDestinationIndexPath.row) {
+            return sourceIndexPath
+        } else {
+            return proposedDestinationIndexPath
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath)  {
+        /*
+        if isEqualToAllItemsCount(row: destinationIndexPath.row) || isEqualToAllItemsCount(row: sourceIndexPath.row) {
+            let alert = UIAlertController (
+                title: "Information",
+                message: "The last line is always 'No more items!' So you can't move this.",
+                preferredStyle: .alert
+            )
+            let okAction = UIAlertAction (
+                title: "OK",
+                style: UIAlertActionStyle.default
+            )
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
+        } else {*/
         itemStore.moveItemAtIndex(fromIndex: sourceIndexPath.row, toIndex: destinationIndexPath.row)
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
+        return "Remove"     // change the title 'Delete' to 'Remove'
+    }
+    
+    func isEqualToAllItemsCount(row : Int) -> Bool {
+        if row == itemStore.allItems.count {
+            return true
+        } else {
+            return false
+        }
     }
     
 }
