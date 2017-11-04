44fecb26b02b72b9cf48d160dd17a99878996264
djflsdl08 <djflsdl08@naver.com>
2017-11-04 22:40:21 +0900

    Added stopwatchView

--- Timer/Timer/StopwatchViewController.swift
+++ Timer/Timer/StopwatchViewController.swift
@@ -11,34 +11,62 @@ import UIKit
 class StopwatchViewController: UIViewController {
 
     @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var lapResetButton: UIButton!
     
+    var timer = Timer()
+    var isTimerRunning = false
+    var seconds = 0
     
     @IBAction func startStopButton(_ sender: UIButton) {
+        lapResetButton.isEnabled = true
+        
+        if sender.currentTitle == "Start" {
+            runTimer()
+            sender.setTitle("Stop", for: .normal)
+            sender.setTitleColor(.red, for: .normal)
+            lapResetButton.setTitle("Lap", for: .normal)
+        } else if sender.currentTitle == "Stop" {
+            timer.invalidate()
+            sender.setTitle("Start", for: .normal)
+            sender.setTitleColor(.green, for: .normal)
+            lapResetButton.setTitle("Reset", for: .normal)
+        }
     }
     
     @IBAction func lapResetButton(_ sender: UIButton) {
+        if sender.currentTitle == "Lap" {
+            
+        } else if sender.currentTitle == "Reset" {
+            timer.invalidate()
+            seconds = 0
+            timerLabel.text = timerLabelFormat(time: TimeInterval(seconds))
+            lapResetButton.isEnabled = false
+            lapResetButton.setTitle("Lap", for: .normal)
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        lapResetButton.isEnabled = false
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func timerLabelFormat(time: TimeInterval) -> String {
+        
+        let date = NSDate(timeIntervalSince1970: Double(time))
+        let formatter = DateFormatter()
+        formatter.timeZone = NSTimeZone(name: "UTC") as TimeZone!
+        formatter.dateFormat = "HH:mm:ss"
+        
+        return formatter.string(from: date as Date)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(StopwatchViewController.updateTimer)), userInfo: nil, repeats: true)
     }
-    */
-
+    
+    func updateTimer() {
+        seconds += 1
+        timerLabel.text = timerLabelFormat(time: TimeInterval(seconds))
+    }
 }
