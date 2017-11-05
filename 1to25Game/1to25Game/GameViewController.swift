636cd3434cc2af015371835b86eb1f7c9af95be2
djflsdl08 <djflsdl08@naver.com>
2017-11-05 21:25:44 +0900

    Added current record.

--- 1to25Game/1to25Game/GameViewController.swift
+++ 1to25Game/1to25Game/GameViewController.swift
@@ -15,6 +15,9 @@ class GameViewController: UIViewController {
     @IBOutlet weak var highestScore: UILabel!
     @IBOutlet weak var currentRecord: UILabel!
     
+    var timer = Timer()
+    var seconds = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
@@ -25,20 +28,37 @@ class GameViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        timer.invalidate()
     }
-    */
+    
     
     //MARK: - Actions
     @IBAction func DisappearTheCover(_ sender: UIButton) {
         pressToStart.removeFromSuperview()
+        runTimer()
     }
     
+    func currentRecordFormat(time: TimeInterval) -> String {
+        
+        let date = NSDate(timeIntervalSince1970: Double(time))
+        let formatter = DateFormatter()
+        formatter.timeZone = NSTimeZone(name: "UTC") as TimeZone!
+        formatter.dateFormat = "HH:mm:ss"
+        
+        return formatter.string(from: date as Date)
+    }
     
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(GameViewController.updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    func updateTimer() {
+        seconds += 1
+        currentRecord.text = currentRecordFormat(time: TimeInterval(seconds))
+    }
 }
