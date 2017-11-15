7012b87399f696901a138a4790301bc9a7572ac3
djflsdl08 <djflsdl08@naver.com>
2017-11-15 22:06:56 +0900

    Added UI of history view.

---
+++ Calculator/Cal/DetailLogViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailLogViewController.swift
+//  Cal
+//
+//  Created by 김예진 on 2017. 11. 15..
+//  Copyright © 2017년 Kim,Yejin. All rights reserved.
+//
+
+import UIKit
+
+class DetailLogViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
